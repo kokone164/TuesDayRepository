7d60863b2de185eb4e8704a9debf575ece68bd15
kokone164 <degawakokone@outlook.jp>
2026-06-30 16:03:13 +0900

    参照渡し

--- 20260630/20260630/20260630.cpp
+++ 20260630/20260630/20260630.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
-using namespace std;
+
+double power(double x, int n);
 
 int main(void)
 {
+	double x;
+	double a = 3.2;
+	int b = 5;
+
+	x = power(a, b);
+	std::cout << "Result:" << x << std::endl;
 	return 0;
+}
+
+double power(double x, int n)
+{
+	double result = 1.0;
+	while (n-- > 0)
+	{
+		result *= x;
+	}
+	return result;
 }
