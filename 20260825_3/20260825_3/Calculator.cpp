9365b651559ca9324a01c2fd5688e0b5b356687c
Roll <degawakokone@outlook.jp>
2026-09-07 12:11:47 +0900

    途中

---
+++ 20260825_3/20260825_3/Calculator.cpp
@@ -0,0 +1,8 @@
+#include "Calculator.h"
+#include<iostream>
+using namespace std;
+
+void Calculator::add()
+{
+
+}
---
+++ 20260825_3/20260825_3/Calculator.h
@@ -0,0 +1,13 @@
+#pragma once
+class Calculator
+{
+public:
+	int num1;
+	int num2;
+
+	void add();
+	void subtract();
+	void multiply();
+	void divide();
+};
+
